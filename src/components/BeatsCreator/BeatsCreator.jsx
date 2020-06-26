c2ed009ae4c841a5bf155915737c3074472ecfa2
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-06-26 17:56:12 -0400

    refactor beats creator & grid

--- src/components/BeatsCreator/BeatsCreator.jsx
+++ src/components/BeatsCreator/BeatsCreator.jsx
@@ -1,42 +1,16 @@
 import styled from '@emotion/styled/macro'
-import React, {useEffect, useState} from "react";
-import Beat from "../Beat/Beat";
+import React, {useEffect, useRef, useState} from "react";
 import Button from "../Button/Button";
 import Box from "../BoxWithCenteredText/Box";
 import {tracks} from "../../assets/sounds/tracks";
+import BeatsCreatorGrid from "../BeatsCreatorGrid/BeatsCreatorGrid";
 
-const separation = '0.5em';
-const totalMeasures = 20;
-
-const BeatsCreatorGrid = styled.div`
-    display: grid;
-    grid-gap: ${separation};
-    grid-template-columns: 1fr 10fr;
-    grid-template-areas: "trackInstrument trackBeats";
-`;
-
-const TrackInstrumentsContainer = styled.div`
-    display: grid;
-    grid-gap: ${separation};
-    grid-area: trackInstrument;
-`;
-
-const TrackInstrument = styled.div`
-    align-items: center;
-    display: flex;
-`;
-
-const TrackBeats = styled.div`
-    display: grid;
-    grid-gap: ${separation};
-    grid-area: trackBeats;
-    grid-template-columns: repeat(${totalMeasures},  1fr);
-`;
+const totalBeats = 20;
 
 const TrackControls = styled.div`
     margin-top: 1em;
+    grid-gap: 1em;
     display: grid;
-    grid-gap: ${separation};
     grid-template-columns: 60px 60px 60px;
 `;
 
@@ -46,14 +20,15 @@ const TrackControl = styled.div`
     justify-content: center;
 `;
 
+
 const BeatsCreator = () => {
+    const beatsContainer = useRef([]);
     const [currentBeat,setCurrentBeat] = useState(0);
-    const [beats,setBeats] = useState([]);
     const [player, setPlayer] = useState();
 
     useEffect(()=>{
-        for(let i = 0; i < totalMeasures; i++){
-            setBeats(beats => [...beats,{}]);
+        for(let i = 0; i < totalBeats; i++){
+            beatsContainer.current.push({})
         }
     },[]);
 
@@ -71,12 +46,12 @@ const BeatsCreator = () => {
         let beat = 0;
         setPlayer(window.setInterval(function()
         {
-            for (let [, value] of Object.entries(beats[beat])) {
+            for (let [, value] of Object.entries(beatsContainer.current[beat])) {
                 value.play();
             }
 
             beat += 1;
-            if(beat === beats.length){
+            if(beat === beatsContainer.current.length){
                 beat=0;
             }
             setCurrentBeat(beat);
@@ -84,36 +59,18 @@ const BeatsCreator = () => {
     }
 
     const toggleBeat = (trackName,beatIndex) => {
-        if (beats[beatIndex][trackName] === undefined){
-            beats[beatIndex][trackName] = new Audio(tracks[trackName].sound);
+        if (beatsContainer.current[beatIndex][trackName] === undefined){
+            beatsContainer.current[beatIndex][trackName] = new Audio(tracks[trackName].sound);
         }else{
-            delete beats[beatIndex][trackName]
+            delete beatsContainer.current[beatIndex][trackName]
         }
     };
 
-    function renderBeats(trackName, totalMeasures) {
-        const beatsSquares = [];
-        for (let i = 0; i < totalMeasures; i++) {
-            beatsSquares.push(<Beat key={`trackName-${i}`} trackName={trackName} beatIndex={i} clickHandler={toggleBeat}/>);
-        }
-        return beatsSquares;
-    }
 
     return (
         <div>
             <Box>Make your own beat!</Box>
-            <BeatsCreatorGrid>
-                <TrackInstrumentsContainer>
-                    {Object.keys(tracks).map((track) =>
-                        <TrackInstrument id={{track}}>
-                            {track}
-                        </TrackInstrument>
-                    )}
-                </TrackInstrumentsContainer>
-                <TrackBeats>
-                    {Object.keys(tracks).map((track) => renderBeats(track,totalMeasures))}
-                </TrackBeats>
-            </BeatsCreatorGrid>
+            <BeatsCreatorGrid totalBeats={totalBeats} tracks={tracks} toggleBeat={toggleBeat} />
             <TrackControls>
                 <TrackControl>
                     <Button variant={'secondary'} onClick={play}>Play</Button>
---
+++ src/components/BeatsCreatorGrid/styles.js
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled/macro";
+
+const separation = '0.5em';
+
+export const TrackInstrumentsContainer = styled.div`
+    display: grid;
+    grid-gap: ${separation};
+    grid-area: trackInstrument;
+`;
+
+export const TrackInstrument = styled.div`
+    align-items: center;
+    display: flex;
+`;
+
+export const TrackBeats = styled.div`
+    display: grid;
+    grid-gap: ${separation};
+    grid-area: trackBeats;
+    grid-template-columns: repeat(${props => props.totalBeats},  1fr);
+`;
+
