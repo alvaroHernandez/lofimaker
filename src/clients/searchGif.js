1d5df5a500550a03eff1ad4a9ad238339bb4069c
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-07-12 14:27:25 -0400

    using separate backend instead of direct api queries

--- src/clients/searchGif.js
+++ src/clients/searchGif.js
@@ -1,9 +1,7 @@
 import {client} from './baseClient';
 
 function searchGif(query) {
-  return client(
-    `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=25&offset=0&rating=G&lang=en&q=${query}`,
-  );
+  return client(`${process.env.REACT_APP_BACKEND_URL}gifs/search?q=${query}`);
 }
 
 export {searchGif};
