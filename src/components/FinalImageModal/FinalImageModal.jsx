08dadd97b1bb2e45130570465236e7c1d23a6892
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-08-01 17:03:42 -0400

    responsive preview

--- src/components/FinalImageModal/FinalImageModal.jsx
+++ src/components/FinalImageModal/FinalImageModal.jsx
@@ -1,5 +1,8 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
 import {DialogContent, DialogOverlay} from '@reach/dialog';
 import React from 'react';
+import {landscape, medium} from '../../styles/mediaqueries';
 
 function FinalImageModal({setIsDialogOpen, isDialogOpen, image, imageFilter}) {
   return (
@@ -10,8 +13,16 @@ function FinalImageModal({setIsDialogOpen, isDialogOpen, image, imageFilter}) {
       style={{zIndex: '1', background: 'hsla(0, 0%, 0%, 0.9)'}}
     >
       <DialogContent
+        css={{
+          width: '100%',
+          [medium]: {width: '100vh'},
+          [landscape]: {width: '100vh'},
+        }}
         aria-label={'lofi preview'}
-        style={{background: 'hsla(0, 0%, 0%, 0.9)'}}
+        style={{
+          background: 'hsla(0, 0%, 0%, 0.9)',
+          padding: 0,
+        }}
       >
         <figure className={imageFilter}>
           <img alt={'final cover'} src={image} width={'100%'} />
--- src/styles/mediaqueries.js
+++ src/styles/mediaqueries.js
@@ -1,1 +1,2 @@
 export const medium = '@media (min-width: 768px)';
+export const landscape = '@media screen and (orientation:landscape)';
