9ec6040f6883c5e9d5efa7c0434f5c516c1cb18b
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-09-12 23:43:39 -0300

    cancel all transport events after changin from one page to another

--- src/App.js
+++ src/App.js
@@ -3,29 +3,33 @@ import LoFiMaker from './components/LofiMaker/LoFiMaker';
 import {PlayersProvider} from './contexts/PlayersContext';
 import {ImageProvider} from './contexts/ImageContext';
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import Gallery from './components/Gallery/Gallery';
+import { Transport } from "tone";
 
 function App() {
+  const history = useHistory();
+  history.listen((location, action) => {
+    Transport.cancel();
+  });
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <ImageProvider>
-            <PlayersProvider>
-              <LoFiMaker />
-            </PlayersProvider>
-          </ImageProvider>
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <ImageProvider>
+          <PlayersProvider>
+            <LoFiMaker />
+          </PlayersProvider>
+        </ImageProvider>
+      </Route>
 
-        <Route exact path="/gallery">
-          <ImageProvider>
-            <PlayersProvider>
-              <Gallery />
-            </PlayersProvider>
-          </ImageProvider>
-        </Route>
-      </Switch>
-    </Router>
+      <Route exact path="/gallery">
+        <ImageProvider>
+          <PlayersProvider>
+            <Gallery />
+          </PlayersProvider>
+        </ImageProvider>
+      </Route>
+    </Switch>
   );
 }
 
